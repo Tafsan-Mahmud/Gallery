89c396f106e7c878c20d64866fc8a351e5d6779b
Tafsan Mahmud Nobin <tafsanmahmudnobin090@gmail.com>
2021-06-10 22:16:43 +0600

    change colour

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ImageCard from './components/ImageCard';
 import ImageSearch from './components/ImageSearch';
+import './components/responsive.css';
 
 
 function App() {
@@ -20,16 +21,20 @@ function App() {
   }, [term]);
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto text">
+        <h1>You Can Search Any Type of Photo</h1>
       <ImageSearch searchText={(text) => setTerm(text)} />
 
-      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
 
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
-        {images.map(image => (
-          <ImageCard key={image.id} image={image} />
-        ))}
-      </div>}
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> :
+        <div id="grid-cstm">
+
+          {
+            images.map(image =><ImageCard key={image.id} image={image} />)
+          }
+
+        </div>}
     </div>
   );
 }
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,10 +4,10 @@ const ImageCard = ({ image }) => {
   const tags = image.tags.split(',');
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="text max-w-sm m-4 rounded overflow-hidden shadow-lg">
       <img src={image.webformatURL} alt="" className="w-full"/>
       <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
+        <div className="font-bold text-purple-700 text-xl mb-2">
           Photo by {image.user}
         </div>
         <ul>
